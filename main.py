904a91ed76b67434311929313396cdc5f808a9ea
dilisa25 <dward@brooklynsteamcenter.org>
2025-12-09 10:50:34 -0500

    browse begining

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from flask import Flask, render_template 
 
+import pymysql
+
+from dynaconf import Dynaconf
+
 app = Flask(__name__)
 
+ 
+config = Dynaconf(settings_file=["settings.toml"])
 
+def connect_db():
+    conn = pymysql.connect(
+        host="db.steamcenter.tech",
+        user="dward",
+        password= config.password,
+        database="dward_doughi_sweets",
+        autocommit="True",
+        cursorclass= pymysql.cursors.DictCursor
+    )
+    return conn
 
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
+
+@app.route("/browse")
+def browse():
+    connection = connect_db
+
+    cursor = connection.cursor 
+
+    cursor.execute("SELECT * FROM `Product` " )
+
+    result = cursor.fetchall()
+    
+    connection.close
+
+    return render_template("browse.html.jinja")
+
